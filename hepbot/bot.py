273c936f423d1ea9d3fcebe2bc6a7e7f394c2456
kreczko <lkreczko@googlemail.com>
2016-09-25 19:09:08 +0100

    pep8 formatting

--- hepbot/bot.py
+++ hepbot/bot.py
@@ -15,7 +15,8 @@ PLUGINS = [
 PLUGINS = ['slackbot.plugins']
 
 LOCATION = os.path.dirname(__file__)
-SETUP_FILE = os.path.join(LOCATION, 'slackbot_settings.py') 
+SETUP_FILE = os.path.join(LOCATION, 'slackbot_settings.py')
+
 
 def setup():
     LOG.info("Setting up")
@@ -25,16 +26,19 @@ def setup():
         import sys
         sys.exit(-1)
     plugins = os.environ.get('PLUGINS', '')
-    
+
     if plugins:
         PLUGINS.extend(plugins.split(','))
-    
-    plugins = ['"{0}"'.format(p) for p in PLUGINS]    
-    file_content = template.format(API_TOKEN=API_TOKEN, PLUGINS=',\n'.join(plugins))
-    
-    
+
+    plugins = ['"{0}"'.format(p) for p in PLUGINS]
+    file_content = template.format(
+        API_TOKEN=API_TOKEN,
+        PLUGINS=',\n'.join(plugins)
+    )
+
     with open(SETUP_FILE, 'w+') as f:
         f.write(file_content)
+
 
 def run():
     # run setup
